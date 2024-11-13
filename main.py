e78c6fde3aad5c40cfc82b1f2ced895464b1af41
YarLys <102531135+YarLys@users.noreply.github.com>
2024-11-13 17:57:17 +0300

    everything works

--- main.py
+++ main.py
@@ -1,40 +1,50 @@
 import os
-import graphviz
+import subprocess
 
 file_hash = r''
 repo_path = r''
+viz_path = r''
 prog_path = r''
 commits = dict()
+
+
 def init(path):
     try:
         with open(path) as f:
-            prog_path = f.readline().split()[1].replace('\\', '\\\\')
+            viz_path = f.readline().split()[1].replace('\\', '\\\\')
             repo_path = f.readline().split()[1].replace('\\', '\\\\')
             file_hash = f.readline().split()[1]
+            global prog_path
+            prog_path = os.popen('cd').read()[:-1]
             os.chdir(repo_path)
-            return (prog_path, repo_path, file_hash)
-    except: # файл не найден
+            return (viz_path, repo_path, file_hash)
+    except:  # файл не найден
         return ("", "", "")
 
+
 def get_commits():
-    output = os.popen('git log --all --pretty=format:"%H %P %s%d"').read().split("\n") # получим список всех коммитов в нужном нам формате
+    output = os.popen('git log --all --pretty=format:"%H %P %s%d"').read().split("\n")  # получим список всех коммитов в нужном нам формате
     for commit in output:
         commit = commit.split()
-        if len(commit) == 2: # если только хэш коммита и подпись, то это начальный коммит
+        if len(commit) == 2:  # если только хэш коммита и подпись, то это начальный коммит
             commits[commit[0]] = ["", commit[1]]
             continue
 
-        commits[commit[0]] = [commit[1],] # в словаре по хэшу коммита храним список, который начинается с хэша родителя
+        commits[commit[0]] = [
+            commit[1], ]  # в словаре по хэшу коммита храним список, который начинается с хэша родителя
         str = ''
-        for i in range(2, len(commit)): # пройдёмся по оставшимся словам в информации о коммите
-            if (not commit[i].startswith('(')): # ненужную информацию убираем
+        for i in range(2, len(commit)):  # пройдёмся по оставшимся словам в информации о коммите
+            if (not commit[i].startswith('(')):  # ненужную информацию убираем
                 str = str + ' ' + commit[i]
             else:
                 break
         commits[commit[0]].append(str)
     return commits
 
+
 true_commits = dict()
+
+
 def check_dirs(commit, hash1):
     output = os.popen(f'git cat-file -p {hash1}').read().split('\n')
     for str in output:
@@ -45,48 +55,51 @@ def check_dirs(commit, hash1):
         naming = str.split()[3]
         commits[commit].append(naming)
         if name == 'blob':
-            if hash == file_hash or hash.startswith(file_hash):  # если его хэш совпадает с хэшом искомого файла, то добавляем этот коммит
+            if hash == file_hash or hash.startswith(
+                    file_hash):  # если его хэш совпадает с хэшом искомого файла, то добавляем этот коммит
                 true_commits[commit] = commits[commit]
                 break
         elif name == 'tree':
             check_dirs(commit, hash)
 
+
 def select_commits():
     for commit in commits:
-        output = os.popen(f'git cat-file -p {commit}').read().split('\n') # исследуем коммит по его хэшу
-        obj = output[0].split()[0] # сохраним название объекта в коммите
-        hash = output[0].split()[1] # сохраним хэш объекта
+        output = os.popen(f'git cat-file -p {commit}').read().split('\n')  # исследуем коммит по его хэшу
+        obj = output[0].split()[0]  # сохраним название объекта в коммите
+        hash = output[0].split()[1]  # сохраним хэш объекта
 
-        if obj == 'blob':   # если в коммите только один файл, без каталогов
-            if hash == file_hash or hash.startswith(file_hash): # и если его хэш совпадает с хэшом искомого файла, то добавляем этот коммит
+        if obj == 'blob':  # если в коммите только один файл, без каталогов
+            if hash == file_hash or hash.startswith(
+                    file_hash):  # и если его хэш совпадает с хэшом искомого файла, то добавляем этот коммит
                 true_commits[commit] = commits[commit]
                 continue
 
-        check_dirs(commit, hash) # пройдёмся по объектам коммита. проверим все файлы на совпадение
+        check_dirs(commit, hash)  # пройдёмся по объектам коммита. проверим все файлы на совпадение
     return true_commits
+
 
 def build_graph():
     dot = []
     dot.append('digraph {')
-    for commit1 in true_commits: # проходимся по словарю
+    for commit1 in true_commits:  # проходимся по словарю
         for commit2 in true_commits:
-            if true_commits[commit2][0] == commit1: # если хэш родителя какого-то коммита равен хэшу другого коммита, то они связаны
-                dot.append(f'"{commit1 + '\n' + str(true_commits[commit1][2:])}" -> "{commit2 + '\n' + str(true_commits[commit2][2:])}" [label="{' '.join(true_commits[commit2][1])}"]')
+            if true_commits[commit2][0] == commit1:  # если хэш родителя какого-то коммита равен хэшу другого коммита, то они связаны
+                dot.append(
+                    f'"{commit1 + '\n' + str(true_commits[commit1][2:])}" -> "{commit2 + '\n' + str(true_commits[commit2][2:])}" [label="{' '.join(true_commits[commit2][1])}"]')
     dot.append('}')
     return '\n'.join(dot)
 
+
 def show_graph(dot_graph):
-    # Создаем объект графа
-    graph = graphviz.Source(dot_graph)
-    # Рендерим граф в виде изображения
-    graph.format = 'png'
-    graph.render(filename='dependencies', cleanup=True)
-    # Открываем изображение
-    graph.view()
+    with open('dependencies.txt', 'w') as file:
+        file.writelines(dot_graph)
+    subprocess.run(f'{viz_path} -Tpng dependencies.txt -o dependencies.png', shell=True)
+    os.popen('dependencies.png')
 
 
 if __name__ == "__main__":
-    (prog_path, repo_path, file_hash) = init('config.yaml')
+    (viz_path, repo_path, file_hash) = init('config.yaml')
     get_commits()
     select_commits()
     graphv = build_graph()
